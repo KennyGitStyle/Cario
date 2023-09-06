318e7add94b54d4f4f89bd3769f8c1b20098f3b8
Kenneth Hendricks <kennethhendricks10@gmail.com>
2023-09-06 16:40:07 +0100

    Create, Edit and Get Auction nextjs

--- frontend/web-app/app/auctions/AuctionsCards.tsx
+++ frontend/web-app/app/auctions/AuctionsCards.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import CountdownTimer from './CountdownTimer';
 import CarImage from './CarImage';
 import { Auction } from '@/types';
+import Link from 'next/link';
 
 type Props = {
   auction: Auction;
@@ -9,7 +10,7 @@ type Props = {
 
 export default function AuctionsCards({ auction }: Props) {
   return (
-    <a href="">
+    <Link href={`/auctions/details/${auction.id}`}>
     <div className="w-full bg-gray-200 aspect-w-16 aspect-h-10 rounded-lg overflow-hidden lg:w-auto relative">
       <div> 
         <CarImage imageUrl={auction.imageUrl} />
@@ -23,6 +24,6 @@ export default function AuctionsCards({ auction }: Props) {
           <h3 className="text-gray-700 text-sm sm:text-base">{auction.make} {auction.model}</h3>
           <p className="font-semibold text-xs sm:text-sm">{auction.year}</p>
     </div>
-    </a>
+    </Link>
   );
 }
